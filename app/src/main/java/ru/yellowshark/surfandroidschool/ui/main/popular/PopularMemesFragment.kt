7a0365a1ae9132c4e3f508b58613aa3f0bde7de9
Daniil Dobrodeev <danil.dobrodeev@mail.ru>
2020-11-02 23:13:58 +0300

    create item meme

--- app/src/main/java/ru/yellowshark/surfandroidschool/ui/main/popular/PopularMemesFragment.kt
+++ app/src/main/java/ru/yellowshark/surfandroidschool/ui/main/popular/PopularMemesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.fragment_popular_memes.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,7 +12,6 @@ import retrofit2.Response
 import ru.yellowshark.surfandroidschool.R
 import ru.yellowshark.surfandroidschool.data.network.MemesApi
 import ru.yellowshark.surfandroidschool.data.network.popular.response.Meme
-import ru.yellowshark.surfandroidschool.utils.WrappableGridLayoutManager
 
 class PopularMemesFragment : Fragment(R.layout.fragment_popular_memes) {
 
@@ -48,7 +48,7 @@ class PopularMemesFragment : Fragment(R.layout.fragment_popular_memes) {
     }
 
     private fun initUi() {
-        val gridLayoutManager = WrappableGridLayoutManager(context?.applicationContext, 2)
+        val gridLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
         memesAdapter.onItemClick = {
             Toast.makeText(activity, "${it.title} was clicked", Toast.LENGTH_SHORT).show()
